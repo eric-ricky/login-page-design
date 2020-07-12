cc98a79141a1388ad8f37dcd39e106fbd108c8a3
Eric Ricky <ericricky200@gmail.com>
2020-07-12 15:59:51 +0300

    placeholder animation

---
+++ main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function() {    
+    $(".txtbox input").on("focus", function() {
+        $(this).addClass("focus");
+    });
+    $(".txtbox input").on("blur", function() {
+        if($(this).val() == "") {
+            $(this).removeClass("focus");
+        }
+        
+    });
+})
