a3dfb1611e706d1e76c089609678e16ff80936b3
Eric Ricky <ericricky200@gmail.com>
2020-07-13 06:49:59 +0300

    alert modal

--- main.js
+++ main.js
@@ -6,7 +6,19 @@ $(document).ready(function() {
     $(".textbox input").on("blur", function() {
         if($(this).val() == "") {
             $(this).removeClass("focus");
+        }        
+    });
+
+    // modal
+    $("input.btn").on("click", function() {
+        if($(".textbox input").val() !== "") {
+            $(".modal").css("display","block");
+        } else {
+            alert('INPUT FIELD CANNOT BE EMPTY');
         }
         
+    });
+    $(".modal-content .close, .modal").on("click", function() {
+        $(".modal").css("display","none");        
     });
 })
